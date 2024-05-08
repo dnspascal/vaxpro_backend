c38ad496eed2be4398f2ae385454ffd61ea84e6f
dnspascal <dnspascal094@gmail.com>
2024-05-08 16:11:35 +0300

    unguja kati district are arranged properly

--- routes/api.php
+++ routes/api.php
@@ -25,73 +25,72 @@ Route::delete('deleteVaccine/{id}', [VaccinationController::class, 'deleteVaccin
 Route::post('parentChildData', [ChildController::class, 'parentChildData']);
 Route::get('generateSchedule', [VaccinationSchedulesController::class, 'vaccine']);
 Route::get('getChildParentData', [ChildController::class, 'getChildParentData']);
-Route::get('children', [ChildController::class,'children']);
-Route::get('parents', [ParentController::class,'parents']);
+Route::get('children', [ChildController::class, 'children']);
+Route::get('parents', [ParentController::class, 'parents']);
 Route::get('getChildData/{id}', [ChildController::class, 'getChildData']);
 
 
 
-Route::middleware('auth:sanctum')->group(function () {
-    
+Route::middleware(['auth:sanctum', 'tokenExpiration'])->group(function () {
+
     //auth endpoints
-    Route::post('/register', [AuthController::class,'register']);
-    Route::patch('update_user/{id}', [AuthController::class,'update']);
-    Route::post('/logout', [AuthController::class,'logout']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::patch('update_user/{id}', [AuthController::class, 'update']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 
     //user endpoints
     Route::get('/user', [UserController::class, 'userData']);
-    Route::delete('/user/{id}', [UserController::class,'destroy']);
-    Route::get('/all_users/{id}', [UserController::class,'allUsers']);
+    Route::delete('/user/{id}', [UserController::class, 'destroy']);
+    Route::get('/all_users/{id}', [UserController::class, 'allUsers']);
 
 
     //address endpoints
-    Route::get('/regions', [RegionController::class,'showAll']);
-    Route::get('region_districts/{region_id}', [DistrictController::class,'region_districts']);
-    Route::get('districts_wards/{district_id}', [WardController::class,'districts_wards']);
+    Route::get('/regions', [RegionController::class, 'showAll']);
+    Route::get('region_districts/{region_id}', [DistrictController::class, 'region_districts']);
+    Route::get('districts_wards/{district_id}', [WardController::class, 'districts_wards']);
 
     //roles endpoints
-    Route::get("/roles",[RoleController::class,"index"]);
+    Route::get("/roles", [RoleController::class, "index"]);
 });
 
 
 
 // regions endpoints
-Route::post('region', [RegionController::class,'create']);
-Route::get('region/{id}', [RegionController::class,'show']);
-Route::put('region/{id}', [RegionController::class,'update']);
-Route::delete('region/{id}', [RegionController::class,'destroy']);
+Route::post('region', [RegionController::class, 'create']);
+Route::get('region/{id}', [RegionController::class, 'show']);
+Route::put('region/{id}', [RegionController::class, 'update']);
+Route::delete('region/{id}', [RegionController::class, 'destroy']);
 
 // district endpoints
-Route::get('district_facilities/{id}', [DistrictController::class,'show_facilities']);
-Route::get('district_wards/{id}', [DistrictController::class,'show_wards']);
-Route::get('districts', [DistrictController::class,'showAll']);
-Route::post('district', [DistrictController::class,'create']);
-Route::get('district/{id}', [DistrictController::class,'show']);
-Route::put('district/{id}', [DistrictController::class,'update']);
-Route::delete('district/{id}', [DistrictController::class,'destroy']);
+Route::get('district_facilities/{id}', [DistrictController::class, 'show_facilities']);
+Route::get('district_wards/{id}', [DistrictController::class, 'show_wards']);
+Route::get('districts', [DistrictController::class, 'showAll']);
+Route::post('district', [DistrictController::class, 'create']);
+Route::get('district/{id}', [DistrictController::class, 'show']);
+Route::put('district/{id}', [DistrictController::class, 'update']);
+Route::delete('district/{id}', [DistrictController::class, 'destroy']);
 
 
 // wards endpoints
-Route::get('wards', [WardController::class,'showAll']);
-Route::post('ward', [WardController::class,'create']);
-Route::get('ward/{id}', [WardController::class,'show']);
-Route::put('ward/{id}', [WardController::class,'update']);
-Route::delete('ward/{id}', [WardController::class,'destroy']);
+Route::get('wards', [WardController::class, 'showAll']);
+Route::post('ward', [WardController::class, 'create']);
+Route::get('ward/{id}', [WardController::class, 'show']);
+Route::put('ward/{id}', [WardController::class, 'update']);
+Route::delete('ward/{id}', [WardController::class, 'destroy']);
 
 // facility endpoints
-Route::get('facilities', [FacilityController::class,'showAll']);
-Route::post('facility', [FacilityController::class,'create']);
-Route::get('facility/{id}', [FacilityController::class,'show']);
-Route::put('facility/{id}', [FacilityController::class,'update']);
-Route::delete('facility/{id}', [FacilityController::class,'destroy']);
+Route::get('facilities', [FacilityController::class, 'showAll']);
+Route::post('facility', [FacilityController::class, 'create']);
+Route::get('facility/{id}', [FacilityController::class, 'show']);
+Route::put('facility/{id}', [FacilityController::class, 'update']);
+Route::delete('facility/{id}', [FacilityController::class, 'destroy']);
 
 
 // send sms 
-Route::post('send_sms',[SMSController::class,'sendSms']);
+Route::post('send_sms', [SMSController::class, 'sendSms']);
 
 
 // booking endpoints
-Route::post('add_booking',[BookingController::class,'store']);
+Route::post('add_booking', [BookingController::class, 'store']);
 
 Route::post('/login', [AuthController::class, 'login']);
-
