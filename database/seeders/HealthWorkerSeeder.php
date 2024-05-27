0de324c0d67f3e962df9afa96ceb5d9e2b4c89cd
dnspascal <dnspascal094@gmail.com>
2024-05-27 20:20:23 +0300

    HealthWorkerSeeder was created with its seedings

---
+++ database/seeders/HealthWorkerSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\HealthWorker;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class HealthWorkerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::create([
+            'uid' => "5000-1-1",
+            "role_id" => 1,
+            'contacts' => '+255745884009',
+            'password' => '12345',
+            "facility_id" => "123705-4",
+        ]);
+
+        HealthWorker::create([
+            "staff_id" => "987534235",
+            "first_name" => "Grace",
+            "last_name" => "Hill",
+            "user_id"=> $user->id
+
+        ]);
+    }
+}
