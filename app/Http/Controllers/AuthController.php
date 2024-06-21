ad24e2e1a6f5f855757f906359ba517f52565f6b
dnspascal <dnspascal094@gmail.com>
2024-06-21 16:54:04 +0300

    password recovery with verification code

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\GeneratePasswordHelper;
+use App\Helpers\GenerateRoleIdHelper;
 use App\Models\HealthWorker;
+use App\Models\User;
+use App\Services\SmsService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 use Illuminate\Support\Facades\Hash;
-use App\Models\User;
-use App\Models\Role;
-use App\Helpers\GenerateRoleIdHelper;
-use App\Helpers\GeneratePasswordHelper;
-use App\Services\SmsService;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
@@ -27,98 +25,64 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-       $validate = Validator::make($request->only('contacts'),[
-           'contacts'=>["unique:users,contacts"]
-       ]) ;
+        $validate = Validator::make($request->only('contacts'), ['contacts' => ["unique:users,contacts"]]);
 
-       if($validate->fails()){
-           return response()->json(["error"=>"contacts","message"=>"This contact is already taken"],400);
-       }
+        if ($validate->fails()) {
+            return response()->json(["error" => "contacts", "message" => "This contact is already taken"], 400);
+        }
 
         if ($request->has("ward_id")) {
 
-            if (User::where('role_id', $request->input('role_id'))
-                ->Where('ward_id', $request->input('ward_id'))
-                ->doesntExist()
-            ) {
+            if (User::where('role_id', $request->input('role_id'))->Where('ward_id', $request->input('ward_id'))->doesntExist()) {
                 $uid = GenerateRoleIdHelper::generateRoleId($request->account_type, null, null, $request->ward_id);
             }
             // else if (Role::where('id', $request->role_id)) {
 
             // }
-             else {
+            else {
                 return response()->json(['message' => 'This account exists ', 'status' => 409]);
             }
         } else if ($request->has("facility_id")) {
-            if (User::where('role_id', $request->input('role_id'))
-                ->Where('facility_id', $request->input('facility_id'))
-                ->doesntExist()
-            ) {
+            if (User::where('role_id', $request->input('role_id'))->Where('facility_id', $request->input('facility_id'))->doesntExist()) {
                 $uid = GenerateRoleIdHelper::generateRoleId($request->account_type, null, 1, null);
-            } else if (
-                $request->account_type == "health_worker"
-            ) {
+            } else if ($request->account_type == "health_worker") {
                 $uid = GenerateRoleIdHelper::generateRoleId($request->account_type, null, 2, null);
-            } else if (
-                $request->account_type == "branch_admin"
-            ) {
+            } else if ($request->account_type == "branch_admin") {
                 $uid = GenerateRoleIdHelper::generateRoleId($request->account_type, null, 2, null);
             } else {
                 return response()->json(["message" => "This account exists ", 'status' => 409]);
             }
         } else if ($request->has("district_id")) {
-            if (User::where('role_id', $request->input('role_id'))
-                ->Where('district_id', $request->input('district_id'))
-                ->doesntExist()
-            ) {
+            if (User::where('role_id', $request->input('role_id'))->Where('district_id', $request->input('district_id'))->doesntExist()) {
                 $uid = GenerateRoleIdHelper::generateRoleId($request->account_type, null, $request->district_id, null);
             } else {
                 return response()->json(["message" => "This district account exists ", 'status' => 409]);
             }
         } else if ($request->has("region_id")) {
-            if (User::where('role_id', $request->input('role_id'))
-                ->Where('region_id', $request->input('region_id'))
-                ->doesntExist()
-            ) {
+            if (User::where('role_id', $request->input('role_id'))->Where('region_id', $request->input('region_id'))->doesntExist()) {
                 $uid = GenerateRoleIdHelper::generateRoleId($request->account_type, $request->region_id, null, null);
             } else {
                 return response()->json(["message" => "This region account exists", 'status' => 409]);
             }
         } else {
-            if (User::where('role_id', $request->input('role_id'))
-                ->doesntExist()
-            ) {
+            if (User::where('role_id', $request->input('role_id'))->doesntExist()) {
                 $uid = GenerateRoleIdHelper::generateRoleId($request->account_type, null, null, null);
             } else {
                 return response()->json(['message' => 'This ministry account exists ', 'status' => 409]);
             }
-        };
+        }
 
 
         $password = GeneratePasswordHelper::generatePassword();
 
         if ($uid && $password) {
             $user = User::find($uid);
-            $user = User::create([
-                'uid' => $uid,
-                "role_id" => $request->role_id,
-                'password' => Hash::make($password),
-                'ward_id' => $request->ward_id,
-                'district_id' => $request->district_id,
-                'region_id' => $request->region_id,
-                'facility_id' => $request->facility_id,
-                'contacts' => $request->contacts,
-            ]);
+            $user = User::create(['uid' => $uid, "role_id" => $request->role_id, 'password' => Hash::make($password), 'ward_id' => $request->ward_id, 'district_id' => $request->district_id, 'region_id' => $request->region_id, 'facility_id' => $request->facility_id, 'contacts' => $request->contacts,]);
 
             if ($request->account_type == "health_worker") {
 
                 if ($request->has("last_name")) {
-                    HealthWorker::create([
-                        'staff_id' => $request->staff_id,
-                        'first_name' => $request->first_name,
-                        'last_name' => $request->last_name,
-                        'user_id' => $user->id
-                    ]);
+                    HealthWorker::create(['staff_id' => $request->staff_id, 'first_name' => $request->first_name, 'last_name' => $request->last_name, 'user_id' => $user->id]);
                 } else {
                     return $request . "REQUEST WITH NO LAST NAME";
                 }
@@ -126,12 +90,10 @@ class AuthController extends Controller
         }
         if ($user) {
             $recipient = $request->input('contacts');
-            $to_user = explode('+',$recipient)[1];
+            $to_user = explode('+', $recipient)[1];
             $postData = [
 
-                'message' => 'Umesajiliwa kikamilifu kwenye mfumo wa VaxPro, tumia password-'." . $password .". " na Profile id " . $user["uid"],
-                'recipient' => $to_user
-            ];
+                'message' => 'Umesajiliwa kikamilifu kwenye mfumo wa VaxPro, tumia password-' . " . $password ." . " na Profile id " . $user["uid"], 'recipient' => $to_user];
 
             // Send SMS using the service
             $this->smsService->sms_oasis($postData);
@@ -141,21 +103,18 @@ class AuthController extends Controller
         }
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, $id): JsonResponse
     {
 
-        $validate = Validator::make($request->only('contacts'),[
-            'contacts'=>["unique:users,contacts"]
-        ]) ;
+        $validate = Validator::make($request->only('contacts'), ['contacts' => ["unique:users,contacts"]]);
 
-        if($validate->fails()){
-            return response()->json(["error"=>"contacts","message"=>"This contact is already taken"],400);
+        if ($validate->fails()) {
+            return response()->json(["error" => "contacts", "message" => "This contact is already taken"], 400);
         }
 
-        $user  =  User::find($id);
+        $user = User::find($id);
 
         if ($request->has('contacts')) {
-
             $user->contacts = $request->contacts;
         }
 
@@ -164,46 +123,35 @@ class AuthController extends Controller
 
         return response()->json(["message" => "user successfully updated"]);
     }
-    public function login(Request $request)
+
+    public function login(Request $request): JsonResponse
     {
         $credentials = $request->only(["uid", "password"]);
 
         if (Auth::attempt($credentials)) {
 
-            $token  = $request->user()->createToken("vaxPro")->plainTextToken;
+            $token = $request->user()->createToken("vaxPro")->plainTextToken;
 
-            return response()->json([
-                "token" => $token,
-                "message" => "logged in",
-                "status" => 200,
-            ]);
+            return response()->json(["token" => $token, "message" => "logged in", "status" => 200,]);
         } else
-            return response()->json([
-                "message" => "user not found",
-                "status" => 404,
-            ]);
+            return response()->json(["message" => "user not found", "status" => 404,]);
     }
 
-    public function parent_login(Request $request)
+    public function parent_login(Request $request): JsonResponse
     {
         $credentials = $request->only(["contacts", "password"]);
 
         if (Auth::attempt($credentials)) {
 
-            $token  = $request->user()->createToken("vaxPro")->plainTextToken;
+            $token = $request->user()->createToken("vaxPro")->plainTextToken;
 
-            return response()->json(
-                $token,
-                 200
-            );
+            return response()->json($token, 200);
         } else
-            return response()->json("Phonenumber or password is incorrect",401);
+            return response()->json("Phonenumber or password is incorrect", 401);
     }
 
 
-
-
-    public function logout(Request $request)
+    public function logout(Request $request): JsonResponse
     {
         $request->user()->tokens()->where('id', $request->user()->currentAccessToken()->id)->delete();
         return response()->json('Logged out successfully', 200);
